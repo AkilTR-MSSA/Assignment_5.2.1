e6426d7ee017c32768f7fcffa12547c705bd5580
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-06 15:22:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(LastWordLength("Hello World"));
+Console.WriteLine(LastWordLength("fly me to the moon"));
+
+int LastWordLength(string input)
+{
+    string[] s = input.Split(' ');
+    return s[s.Length - 1].Length;
+}
